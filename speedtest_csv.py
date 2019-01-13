a6936d6e3750fdd1c33d7280fc71faa2b8b7eee0
Warren Fisher <github@wfisher.info>
2019-01-13 11:41:25 -0400

    Added option to store data in a folder

--- speedtest_csv.py
+++ speedtest_csv.py
@@ -1,12 +1,28 @@
 import speedtest
 import pandas as pd
+import os
 
-filename = './speedtest_data/data.csv'
+dirname = './speedtest_data'
+filename = 'data.csv'
+file_path = dirname + '/' + filename
 port_num = 8888 
+automatic_browser_open = False
+use_web_view = False
 bit_to_megabit_ratio = 1048576**-1
 # Insert a list of specific server IDs from http://www.speedtest.net/speedtest-servers-static.php to allow/block. 
 allowed_servers = []
 blocked_servers = []
+
+def remove_key(d, key):
+	r = dict(d)
+	del r[key]
+	return r
+
+def make_dir(directory): 
+	try: 
+		os.makedirs(directory)
+	except FileExistsError: 
+	return 
 
 s = speedtest.Speedtest()
 s.get_servers(allowed_servers, blocked_servers)
@@ -15,11 +31,6 @@ s.download()
 s.upload()
 
 results_dict = s.results.dict()
-
-def remove_key(d, key):
-	r = dict(d)
-	del r[key]
-	return r
 	
 keys_to_remove = ['server','bytes_sent','bytes_received','share','client','ping']
 for key in keys_to_remove: 
@@ -33,6 +44,7 @@ try:
 	df_old = pd.read_csv(filename, sep=',') 
 	df_final = pd.concat([df_old, df_new], ignore_index = True) # ignore_index prevents an index output of 0, 1, 2, 0 which happens when concatenating two dataframes with index 0, 1, 2 and 0
 except FileNotFoundError: # if a file is not found we want the dataframe to only consist of our latest data
+	make_dir(dirname)
 	df_final = df_new
 
 df_final.to_csv(filename, encoding='utf-8', index=False) # Our index does not matter at this point. It is easier to set the index as timestamp when displaying data
