03d161fd465a30b5a958c1a166130e3034947b21
Warren Fisher <github@wfisher.info>
2019-01-18 16:42:10 -0400

    Fixed speedtest running when attempting to plot speedtest history

--- speedtest_csv.py
+++ speedtest_csv.py
@@ -24,31 +24,36 @@ def make_dir(directory):
 		os.makedirs(directory)
 	except FileExistsError: 
 		return 
+
+def main():		
+	s = speedtest.Speedtest()
+	s.get_servers(allowed_servers, blocked_servers)
+	s.get_best_server()
+	s.download()
+	s.upload()
+
+	results_dict = s.results.dict()
+
+	keys_to_remove = ['server','bytes_sent','bytes_received','share','client','ping']
+	for key in keys_to_remove: 
+		results_dict = remove_key(results_dict, key)
+
+	results_dict['download'] *= bit_to_megabit_ratio
+	results_dict['upload'] *= bit_to_megabit_ratio
+	results_dict['timestamp'] = dt.date.strftime(dt.datetime.now(),'%M:%I%p-%d-%m-%Y')
+
+	df_new = pd.DataFrame([results_dict])
+	try:
+		df_old = pd.read_csv(file_path, sep=',') 
+		df_final = pd.concat([df_old, df_new], ignore_index = True) # ignore_index prevents an index output of 0, 1, 2, 0 which happens when concatenating two dataframes with index 0, 1, 2 and 0
+	except FileNotFoundError: # if a file is not found we want the dataframe to only consist of our latest data
+		make_dir(dirname) 
+		df_final = df_new
+
+	df_final.to_csv(file_path, encoding='utf-8', index=False) # Our index does not matter at this point. It is easier to set the index as timestamp when displaying data
+
+	print(df_final)
 	
-s = speedtest.Speedtest()
-s.get_servers(allowed_servers, blocked_servers)
-s.get_best_server()
-s.download()
-s.upload()
+if __name__=='__main__':
+	main()
 
-results_dict = s.results.dict()
-
-keys_to_remove = ['server','bytes_sent','bytes_received','share','client','ping']
-for key in keys_to_remove: 
-	results_dict = remove_key(results_dict, key)
-
-results_dict['download'] *= bit_to_megabit_ratio
-results_dict['upload'] *= bit_to_megabit_ratio
-results_dict['timestamp'] = dt.date.strftime(dt.datetime.now(),'%M:%I%p-%d-%m-%Y')
-
-df_new = pd.DataFrame([results_dict])
-try:
-	df_old = pd.read_csv(file_path, sep=',') 
-	df_final = pd.concat([df_old, df_new], ignore_index = True) # ignore_index prevents an index output of 0, 1, 2, 0 which happens when concatenating two dataframes with index 0, 1, 2 and 0
-except FileNotFoundError: # if a file is not found we want the dataframe to only consist of our latest data
-	make_dir(dirname) 
-	df_final = df_new
-
-df_final.to_csv(file_path, encoding='utf-8', index=False) # Our index does not matter at this point. It is easier to set the index as timestamp when displaying data
-
-print(df_final)
